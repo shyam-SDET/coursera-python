085d37389cc6949f3c519078d63b9937598d8dfe
Shyam Jat <52708722+shyamkhilery@users.noreply.github.com>
2020-06-21 09:19:22 +0530

    Create assignment5.2.py

---
+++ assignment5.2.py
@@ -0,0 +1,23 @@
+largest = -1
+smallest = None
+
+while True:
+    inp = raw_input("Enter a number: ")
+    if inp == "done" : break
+    try:
+        num = float(inp)
+    except:
+        print ("Invalid input")
+        continue
+    if smallest is None:
+        smallest = num 
+    if num > largest :
+        largest = num
+    elif num < smallest :
+        smallest = num
+
+def done(largest,smallest):
+    print ("Maximum is", int(largest))  
+    print ("Minimum is", int(smallest))
+
+done(largest,smallest)
