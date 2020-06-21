1da4007cab64e8770c1c884f5461e0190d844b43
Shyam Jat <52708722+shyamkhilery@users.noreply.github.com>
2020-06-21 09:12:24 +0530

    Create assignment3.3.py

---
+++ assignment3.3.py
@@ -0,0 +1,12 @@
+score = input("Enter Score: ")
+number=float(score)
+if(number>=0.9):
+    print("A");
+elif(number>=0.8):
+    print("B")
+elif(number>=0.7):
+    print("C")
+elif(number>=0.6):
+    print("D")
+elif(number<0.6):
+    print("F")
